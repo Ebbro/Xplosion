22a687dc33c66a1729fc359ad673df0a5b5b18c9
maxmat888 <massimotricoli031@gmail.com>
2024-05-07 13:36:24 +0200

    add explosive

---
+++ Assets/Scripts/Explosive.cs
@@ -0,0 +1,29 @@
+using UnityEngine; 
+using System.Collections; 
+using System.Collections.Generic;
+public class ExplodeExample : MonoBehaviour { 
+   public float radius = 5.0F; 
+   public float power = 100.0F; 
+   public float lift = 10; 
+   public float speed = 500; 
+   public bool explode = false; 
+   void FixedUpdate() { 
+      if(explode){ 
+         Vector3 explosionPos = transform.position; 
+         Collider[] colliders = Physics.OverlapSphere(explosionPos, radius); 
+         foreach (Collider hit in colliders) { 
+            if (hit.GetComponent<Rigidbody>()) { 
+               hit.GetComponent<Rigidbody>().AddExplosionForce(power, explosionPos, radius, lift); 
+            } 
+         }
+         Destroy(this.gameObject,0.3f); 
+       } 
+   } 
+   void OnCollisionEnter(Collision collision) {    
+      if(collision.gameObject.tag == "Cube"){ 
+         
+         explode = true;
+      }
+   }
+
+}
